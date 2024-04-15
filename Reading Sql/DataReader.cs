7be3d329121efe652c501547ab0c1ad4c1558bdb
Anders Fibiger Jensen <ande153d@edu.zealand.dk>
2024-04-15 11:28:44 +0200

    Add project files.

---
+++ Reading Sql/DataReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.IO;
+
+namespace Reading_Sql
+{
+    public class DataReader
+    {
+        public byte[] ConvertToByte(string path)
+        {
+            var bytes = File.ReadAllBytes(path);
+            return bytes;
+        }
+
+        public void ConvertToImage(byte[] bytes)
+        {
+            using (var ms = new MemoryStream(bytes))
+            {
+                using (var fs = new FileStream("savePath", FileMode.Create))
+                {
+                    ms.WriteTo(fs);
+                }
+            }
+        }
+    }
+}
---
+++ Reading Sql/Image.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reading_Sql
+{
+    public class Image
+    {
+        public int ID { get; set; }
+
+        public byte[] image { get; set; }
+
+        public Image()
+        {
+            
+        }
+
+    }
+}
